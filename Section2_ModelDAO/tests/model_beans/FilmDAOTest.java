d2d45d2bfe90e5b4e2fa7e3329fea3a32c6845b2
mcooling <marccooling@outlook.com>
2021-03-07 22:49:09 +0000

    Testing updates to Section2_ModelDAO Replaced previous manual TestingClass main method, with JUnit test class

---
+++ Section2_ModelDAO/tests/model_beans/FilmDAOTest.java
@@ -0,0 +1,96 @@
+package model_beans;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmDAOTest {
+
+    /**
+     * fetches all films from FilmDAO and prints to console
+     */
+
+    @Test
+    void getAllFilms() {
+
+        FilmDAO filmDAO = new FilmDAO();
+        ArrayList<Film> films = filmDAO.getAllFilms();
+
+        System.out.println("****** TEST BEGINS ******\n");
+
+        for (Film film : films) {
+            System.out.println(
+                    "Film ID: " + film.getId() + "\n" +
+                            "Title: " + film.getTitle() + "\n" +
+                            "Year: " + film.getYear() + "\n" +
+                            "Director: " + film.getDirector() + "\n" +
+                            "Main Cast: " + film.getStars() + "\n" +
+                            "Synopsis: " + film.getReview() + "\n");
+        }
+
+        System.out.println("****** TEST ENDS ******");
+
+    }
+
+    /**
+     * sends a search string into getFilm method<br>
+     * fetches all matching films and prints to console
+     */
+
+    @Test
+    void getFilm() {
+
+        FilmDAO filmDAO = new FilmDAO();
+        ArrayList<Film> films = filmDAO.getFilm("wars");
+
+        System.out.println("****** TEST BEGINS ******\n");
+
+        for (Film film : films) {
+            System.out.println(
+                    "Film ID: " + film.getId() + "\n" +
+                            "Title: " + film.getTitle() + "\n" +
+                            "Year: " + film.getYear() + "\n" +
+                            "Director: " + film.getDirector() + "\n" +
+                            "Main Cast: " + film.getStars() + "\n" +
+                            "Synopsis: " + film.getReview() + "\n");
+        }
+
+        System.out.println("****** TEST ENDS ******");
+
+    }
+
+    /**
+     * creates a Film object and passes into insertFilm method<br>
+     * prints SQL insert executeUpdate response code to console<br>
+     * 1+ = completed insert, 0 = unsuccessful insert
+     */
+
+    @Test
+    void insertFilm() {
+
+        Film film = new Film(
+                101,"My Film",2021,"Me","Me Again",
+                "Load of old rubbish");
+
+        FilmDAO filmDAO = new FilmDAO();
+
+        int returnValue = filmDAO.insertFilm(film);
+
+        System.out.println("****** TEST BEGINS ******\n");
+
+        if (returnValue == 0) {
+            System.out.println("SQL insert failed: " + returnValue + " film records added \n");
+
+        } else if (returnValue == 1) {
+            System.out.println("SQL insert complete: " + returnValue + " film record added \n");
+
+        } else if (returnValue > 1) {
+            System.out.println("SQL insert complete: " + returnValue + " film records added \n");
+        }
+
+        System.out.println("****** TEST ENDS ******");
+
+    }
+}
